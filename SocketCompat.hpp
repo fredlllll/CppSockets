8604b0567cbb571d053e6fba46a93ae3a523a4c8
Frederik Gelder <frederik.gelder@freenet.de>
2022-08-23 03:55:53 +0200

    all kinds of fixing for socketcompat

--- SocketCompat.hpp
+++ SocketCompat.hpp
@@ -8,6 +8,14 @@
 
 #pragma once
 
+#ifdef CPPSOCKETS_DEBUG
+#define CPPSOCKETS_DEBUG_PRINT(X) fprintf(stdout, X)
+#define CPPSOCKETS_DEBUG_PRINT_ERROR(X) fprintf(stderr, X)
+#else
+#define CPPSOCKETS_DEBUG_PRINT(X)
+#define CPPSOCKETS_DEBUG_PRINT_ERROR(X)
+#endif
+
 // Windows
 #ifdef _WIN32
 #pragma comment(lib,"ws2_32.lib")
@@ -33,41 +41,78 @@ static const int SOCKET_ERROR   = -1;
 #include <stdio.h>
 #include <stdint.h>
 
+#ifdef _WIN32
+
+void handleWinapiError(int error) {
+#ifdef CPPSOCKETS_DEBUG
+    LPSTR errorMessagePtr = NULL;
+    FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER, NULL, error, MAKELANGID(LANG_ENGLISH, SUBLANG_DEFAULT), (LPTSTR)&errorMessagePtr, 0, NULL);
+    if (errorMessagePtr) {
+        CPPSOCKETS_DEBUG_PRINT_ERROR(errorMessagePtr);
+        LocalFree(errorMessagePtr);
+    }
+#endif
+}
+
+bool _cppsockets_initWinsockSuccess = false;
+bool initWinsock() {
+    if (_cppsockets_initWinsockSuccess) {
+        return true;
+    }
+    WSADATA wsaData;
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (result != 0) {
+        handleWinapiError(result);
+        return false;
+    }
+    _cppsockets_initWinsockSuccess = true;
+    return true;
+}
+
+bool cleanupWinsock() {
+    if (_cppsockets_initWinsockSuccess) {
+        _cppsockets_initWinsockSuccess = false;
+        int result = WSACleanup();
+        if (result != 0) {
+            handleWinapiError(result);
+            return false;
+        }
+    }
+    return true;
+}
+#endif
+
+void cppSocketsInit() {
+#ifdef _WIN32
+    if (!initWinsock()) {
+        //honestly no idea
+    }
+#endif
+}
+
+void cppSocketsDeinit() {
+#ifdef _WIN32
+    if (!cleanupWinsock()) {
+        //honestly no idea
+    }
+#endif
+}
+
+
 class Socket {
     protected:
         socket_t _sock;
 
-        bool initWinsock(void)
+        static void inetPton(const char * host, struct sockaddr_in & saddr_in)
         {
 #ifdef _WIN32
-            WSADATA wsaData;
-            int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-            if (iResult != 0) {
-                LPSTR errorMessagePtr = NULL;
-                FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER, NULL, iResult, MAKELANGID(LANG_ENGLISH, SUBLANG_DEFAULT), (LPTSTR)&errorMessagePtr, 0, NULL);
-                if (errorMessagePtr) {
-                    // TODO:do something with the message in errorMessage
-                    LocalFree(errorMessagePtr);
-                }
-                return false;
-            }
+#ifdef UNICODE
+            WCHAR host_[64];
+            swprintf_s(host_, L"%S", host);
+#else
+            const char* host_ = host;
 #endif
-            return true;
-        }
-
-        void cleanup(void)
-        {
-#ifdef _WIN32
-            WSACleanup();
-#endif
-        }
-
-        void inetPton(const char * host, struct sockaddr_in & saddr_in)
-        {
-#ifdef _WIN32
-            WCHAR wsz[64];
-            swprintf_s(wsz, L"%S", host);
-            InetPton(AF_INET, wsz,   &(saddr_in.sin_addr.s_addr));
+            InetPton(AF_INET, host_, &(saddr_in.sin_addr.s_addr));
 #else
             inet_pton(AF_INET, host, &(saddr_in.sin_addr));
 #endif
